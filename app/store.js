e84537672396dcb83db14c6bf4a3a882fc804125
shinima <842351815@qq.com>
2016-05-06 15:07:02 +0800

    Add normalize.css. Remove thunk middleware.

--- app/store.js
+++ app/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducer'
 import simpleStringDispatchMiddleware from './simpleStringDispatchMiddleware'
-import thunkMiddleware from 'redux-thunk'
 
 export default createStore(
   reducer,
-  applyMiddleware(simpleStringDispatchMiddleware, thunkMiddleware)
+  applyMiddleware(simpleStringDispatchMiddleware)
 )
